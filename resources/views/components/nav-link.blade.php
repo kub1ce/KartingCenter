9207949fa237c547a085009457ffbcc883008345
Kubice <24kub_yt@mail.ru>
2026-06-02 21:17:13 +0500

    quick fix navbar text desctop

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
  $classes = ($active ?? false)
-            ? 'block w-full ps-3 pe-4 py-2 border-b-4 border-red-500 text-start text-sm font-bold uppercase tracking-wider neon-text-active transition duration-150 ease-in-out'
-            : 'block w-full ps-3 pe-4 py-2 border-b-4 border-transparent text-start text-sm font-bold uppercase tracking-wider text-gray-500 hover:text-lime-400 hover:border-gray-700 transition duration-150 ease-in-out';
+            ? 'block w-full ps-3 pe-4 py-2 border-b-4 border-red-500 text-center text-sm font-bold uppercase tracking-wider neon-text-active transition duration-150 ease-in-out'
+            : 'block w-full ps-3 pe-4 py-2 border-b-4 border-transparent text-center text-sm font-bold uppercase tracking-wider text-gray-500 hover:text-lime-400 hover:border-gray-700 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
